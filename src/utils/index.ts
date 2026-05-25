c31f57a96cc442742e75be46662ed1baeb7c0d39
Iwon <iwongunawan@gmail.com>
2026-05-25 09:14:16 +0700

    utils: month & date format

---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export const formatRupiah = (n: number) => 'Rp.' + n.toLocaleString('id-ID');
+
+export const MONTH_NAMES = [
+  '', 'Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun',
+  'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des',
+];
